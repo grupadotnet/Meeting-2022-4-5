020d4fc1f65a8a3d5c2d04b5cbec0133698cf00c
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2022-11-08 22:12:43 +0100

    model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TutorialASP.Models
+{
+    public class User
+    {
+        public Guid id { get; set; }
+        public string name { get; set; } = String.Empty;
+        public string lastname { get; set; } = String.Empty;
+        public DateOnly birthDate { get; set; }
+        public string place { get; set; } = String.Empty;
+
+    }
+}
