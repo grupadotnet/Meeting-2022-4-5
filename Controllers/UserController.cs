c77b43b09c242571b0d4e1cb083bfebaaa68713b
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2022-11-09 00:02:00 +0100

    controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TutorialASP.Interfaces;
+using TutorialASP.Models;
+
+namespace TutorialASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = _userService.GetUsers();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne(Guid id)
+        {
+            var user = _userService.GetById(id);
+            if(user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserDto createUserDto)
+        {
+            var user = _userService.Create(createUserDto);
+            return Ok(user);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateUserDto updateUserDto)
+        {
+            try
+            {
+                var user = _userService.Update(id, updateUserDto);
+                return Ok(user);
+            } 
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            _userService.Delete(id);
+
+            return NoContent();
+        }
+
+
+    }
+}
