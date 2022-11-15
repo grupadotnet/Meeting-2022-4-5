2192b73390569c6cf34b441588f56448a73e7069
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2022-11-16 00:45:03 +0100

    update model

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,15 @@
-﻿namespace TutorialASP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TutorialASP.Models
 {
     public class User
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; } = String.Empty;
+        [MaxLength(13)]
         public string LastName { get; set; } = String.Empty;
+        [MaxLength(30)] // najdłuższe polskie nazwisko Achmistrowicz-Wachmistrowicz
         public string Place { get; set; } = String.Empty;
 
     }
